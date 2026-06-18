129efaa6d1ae19c7e6fff252948a82d7ab721e73
theLegend0000 <chemistryguider79@gmail.com>
2026-06-18 05:00:48 +0500

    fixing the CORS header problem

--- components/CountryDetail.jsx
+++ components/CountryDetail.jsx
@@ -1,113 +1,105 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import "../style.css"
 import styles from "./CountryDetail.module.css"
-import { useParams } from "react-router"
-import { useTheme } from "../hooks/useTheme"
-import Shimmer2 from "./Shimmer2"
-import { Link } from "react-router-dom"
+import { Link , useParams } from "react-router"
+import ShimmerCardDetail from "./ShimmerCardDetail"
+import { ThemeContext } from "../contexts/ThemeContext"
 
-
+const API_KEY = process.env.RESTCOUNTRIES_API_KEY;
 const CountryDetail = () => {
 
-    //const countryName = new URLSearchParams(location.search).get("name")
     const params = useParams()
-    const countryName = params.country
-    const [countries, setCountries] = useState()
-    const [countryBorders, setBorders] = useState([])
+    const cN = params.country
+    const countryName = cN.charAt(0).toUpperCase() + cN.slice(1);
+    const [countryData, setCountryData] = useState(null)
+    const [countryNotFound, setCountryNotFound] = useState(false)
+    const [isDark] = useContext(ThemeContext)
     const [load, setLoad] = useState(true)
-    const [error, setError] = useState(false)
-    const [isDark, setIsDark] = useTheme()
 
     useEffect(() => {
-
-        fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true&fields=name,flags,region,population,capital,subregion,tld,currencies,languages,borders`)
+        fetch(`https://restcountries.com/v5/name/${cN}?fields=name,flags,region,population,capital,subregion,tld,currencies,languages,borders`,
+        {
+            headers: {
+    Authorization: `Bearer ${API_KEY}`,
+  },
+        })
             .then((res) => res.json())
-            .then((data) => {
-                if (!data || !data.length) {
-                    throw new Response("Country Not Found", { status: 404 });
-                }
-                const country = data[0];
-                setCountries(
-                    {
-                        name: country.name.common,
-                        nativeName: Object.values(country.name.nativeName || {})[0]?.common,
-                        population: country.population,
-                        region: country.region,
-                        subRegion: country.subregion,
-                        capital: country.capital?.join(", ") || "N/A",
-                        topLevelDomain: country.tld,
-                        currencies: Object.values(country.currencies || {}).map((currency) => currency.name).join(', '),
-                        languages: Object.values(country.languages || {}).join(', '),
-                        flags: Object.values(country.flags)[1],
-                        borders: country.borders || [],
-                    }
-
-
-                )
+            .then(([data]) => {
+                setCountryData({
+                    name: data.name.common,
+                    nativeName: Object.values(data.name.nativeName || {})[0].common,
+                    population: data.population,
+                    region: data.region,
+                    subRegion: data.subregion,
+                    capital: Object.values(data.capital).join(', '),
+                    topLevelDomain: data.tld,
+                    currencies: Object.values(data.currencies || {}).map((currency) => currency.name).join(', '),
+                    languages: Object.values(data.languages || {}).join(', '),
+                    flags: Object.values(data.flags)[1],
+                    borders: [],
+                })
                 setLoad(false)
+
+                Promise.all(
+                    data.borders.map((border) =>
+                        fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+                            .then((res) => res.json())
+                            .then(([borderData]) => borderData.name.common)
+                    )
+                ).then((borders) => {
+                    setCountryData((prevState) => ({
+                        ...prevState,
+                        borders
+                    }))
+                })
             })
-            .catch(err => {
-                console.log(err)
-                setError(true)
-            })
-            .finally(() => {
-                setLoad(false)
+            .catch(()=>{
+                setCountryNotFound(true)
             })
     }, [countryName])
 
-    useEffect(() => {
-        if (!countries?.borders?.length) return
 
-        fetch(
-            `https://restcountries.com/v3.1/alpha?codes=${countries.borders.join(",")}`
-        )
-            .then(res => res.json())
-            .then(data => {
-                setBorders(data)
-            })
-
-    }, [countries])
-
-    if (error) {
-        return <>
-            <div style={{padding: "10px"}} className={styles.back}>
-                <button style={{padding: "6px 20px"}} className="montserrat-body" onClick={() => history.back()}>← Back</button>
-            </div>
-            <h1 style={{ textAlign: "center", }} className="montserrat-body">Country Not Found</h1>
-        </>
+    if(countryNotFound){
+        return <div className={['montserrat-body', styles.notFound].join(' ')}>Country Not Found!</div>
     }
+
     return (
-        <main className={[`${isDark ? 'dark' : ''}`, styles.main2].join(' ')}>
-
-            <section className={styles.section3}>
-                <div className={styles.back}>
-                    <button className="montserrat-body" onClick={() => history.back()}>← Back</button>
+        <main className={`${isDark ? 'dark' : ''}`}>
+            {load ? <ShimmerCardDetail /> : 
+        <section className={styles.section3}>
+            <div className={styles.first}>
+                <button className="montserrat-body" onClick={() => history.back()}>Back</button>
+            </div>
+            <div className={styles.second}>
+                <div className={styles.main}>
+                    <h1 className="montserrat-body">{countryData.name}</h1>
+                    <img src={countryData.flags} alt={countryData.name} />
                 </div>
-                {load ? <Shimmer2 />
-                    :
-                    <div className={styles.information}>
-                        <div className={styles.left}>
-                            <h1 className="montserrat-body">{countries.name}</h1>
-                            <img src={countries.flags} alt={countries.name} />
-                        </div>
-
-                        <div className={[styles.right, "montserrat-body"].join(' ')}>
-                            <p><b>Native Name: </b>{countries.nativeName || countries.name}</p>
-                            <p><b>Population: </b>{countries.population.toLocaleString('en-UN')}</p>
-                            <p><b>Region: </b>{countries.region}</p>
-                            <p><b>Sub Region: </b>{countries.subRegion}</p>
-                            <p><b>Capital: </b>{countries.capital}</p>
-                            <p><b>Top Level Domain: </b>{countries.topLevelDomain}</p>
-                            <p><b>Currencies: </b>{countries.currencies}</p>
-                            <p><b>Languages: </b>{countries.languages}</p>
-                            <p><b>Borders: </b>{countryBorders.length ? countryBorders.map((c, i) => (<Link key={i} to={`/${c.name.common.toLowerCase()}`}>{c.name.common} </Link>)) : "None"}</p>
-                        </div>
-                    </div>
-                }
-            </section>
-
+                <div className={['montserrat-body', styles.details].join(' ')}>
+                    <p><b>Native Name: </b>{countryData.nativeName || countryData.name}</p>
+                    <p><b>Population: </b>{countryData.population.toLocaleString('en-UN')}</p>
+                    <p><b>Region: </b>{countryData.region}</p>
+                    <p><b>Sub Region: </b>{countryData.subRegion}</p>
+                    <p><b>Capital: </b>{countryData.capital}</p>
+                    <p><b>Top Level Domain: </b>{countryData.topLevelDomain}</p>
+                    <p><b>Currencies: </b>{countryData.currencies}</p>
+                    <p><b>Languages: </b>{countryData.languages}</p>
+                    <p className={styles.borders}>
+                        <b>Borders: </b>
+                        {countryData.borders.length === 0
+                            ? 'None'
+                            : countryData.borders.map((border) => (
+                                <Link key={border} to={`/${border}`.toLowerCase()}>{border} </Link>
+                            ))
+                        }
+                    </p>
+                </div>
+            </div>
+        </section>
+}
         </main>
     )
+
 }
 
 export default CountryDetail
